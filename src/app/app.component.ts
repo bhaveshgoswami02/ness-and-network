51ca932f6cb02c25943d67101d30aee61d88ca89
jayneet <porwaljayneet97@gmail.com>
2021-05-27 15:55:09 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ness-and-network';
+  cities:any = [];
+
+  selectedCity:any;
+
+  constructor() {
+    this.cities = [
+      { name: 'New York', code: 'NY' },
+      { name: 'Rome', code: 'RM' },
+      { name: 'London', code: 'LDN' },
+      { name: 'Istanbul', code: 'IST' },
+      { name: 'Paris', code: 'PRS' }
+    ];
+  }
 }
